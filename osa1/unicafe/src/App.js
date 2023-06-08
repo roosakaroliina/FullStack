9499c3d087b5607edec08b4688f2bc915d410003
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-08 11:24:21 +0300

    tehtävä 1.7 valmis

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -7,23 +7,40 @@ const Button = (props) => (
 )
 
 
-
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good + 1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(bad + neutral + updatedGood)
+    setAverage((bad*(-1) + updatedGood * 1)/(all+1))
+    setPositive(updatedGood / (all+1) * 100)
+
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(bad + updatedNeutral + good)
+    setAverage((bad*(-1) + good * 1)/(all+1))
+    setPositive(good / (all+1) * 100)
+
   }
   const handleBadClick = () => {
-    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(updatedBad + neutral + good)
+    setAverage((updatedBad*(-1) + good * 1)/(all+1))
+    setPositive(good / (all+1) * 100)
+
   }
+
 
   return (
     <div>
@@ -35,6 +52,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p> average {average} </p>
+      <p>positive {positive} % </p>
     </div>
   )
 }
