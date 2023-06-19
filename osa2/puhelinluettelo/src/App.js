56fda62da97adacac2a3d5d6e4dff78faee0198c
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-19 11:06:13 +0300

    2.9 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 1231244}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState()
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newName,
@@ -23,7 +27,7 @@ const App = () => {
   
     setPersons(persons.concat(noteObject))
     setNewName('')
-    setNewNumber()
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -33,11 +37,24 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
+    const handleFilterChange = (event) => {
+      console.log(event.target.value)
+      setFilter(event.target.value)
+  }
+
+  const filterPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+        filter shown with <input 
+        value={filter}
+        onChange={handleFilterChange}
+        />
+      <h2>add a new</h2>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
           value={newName}
@@ -52,14 +69,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => 
+        {filterPersons.map(person => 
           <p key={person.name}>
             {person.name} {person.number}
           </p>
         )}
     </div>
   )
-
 }
 
 export default App
