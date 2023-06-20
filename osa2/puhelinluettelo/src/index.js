27ea1897f9597dae8f0151ae70dad04ccdc92ebf
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-06-20 12:02:15 +0300

    2.11 valmis

--- osa2/puhelinluettelo/src/index.js
+++ osa2/puhelinluettelo/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import axios from 'axios'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+axios.get('http://localhost:3001/persons').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
+})
