bf4d325c4fb7ad8463d407b86fadf9912e3ca3ad
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-07-25 11:51:47 +0300

    4.5 valmis

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,17 +1,29 @@
 const dummy = (blogs) => {
-    return (
-        1
-    )
-  }
+  return 1
 
-  const totalLikes = (blogs) => {
-    var totalAmount = blogs.reduce((sum, like) => sum + like.likes, 0)
-    return totalAmount
-  }
+}
 
+const totalLikes = (blogs) => {
+  var totalAmount = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return totalAmount
+}
 
-  
-  module.exports = {
-    dummy,
-    totalLikes
+const favoriteBlog = (blogs) => {
+  let maxLikes = blogs[0].likes
+  let mostLikes = blogs[0]
+
+  for (let i = 1; i < blogs.length; i++) {
+    const currentLikes = blogs[i].likes
+    if (currentLikes > maxLikes) {
+      maxLikes = currentLikes
+      mostLikes = blogs[i]
+    }
   }
+  return mostLikes.title
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
