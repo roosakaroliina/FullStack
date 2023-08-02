7ef0c4cf6e0506d48dfdb46ee4b7b7e162f4dc50
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-08-02 12:00:14 +0300

    4.10 valmis

--- osa4/tests/note_api.test.js
+++ osa4/tests/note_api.test.js
@@ -4,6 +4,14 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
@@ -13,7 +21,13 @@ test('blogs are returned as json', async () => {
 
 test('there are two blogs', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(2)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+
+test('the first blog is written by Michael Chan', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].author).toBe('Michael Chan')
 })
 
 test('blogs have an identifier field called id', async () => {
@@ -21,11 +35,23 @@ test('blogs have an identifier field called id', async () => {
   expect(response.body[0].id).toBeDefined()
 })
 
-test('the first blog is written by Rose Griffin', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body[0].author).toBe('Rose Griffin')
-})
+test('Blogs can be added by HTTP POST request', async () => {
+  const newBlog = {
+    title: 'Autumn without you',
+    author: 'Emma Thompson',
+    url: 'http://emmasdiary.blogger.fi/autumnwithoutyou',
+    likes: 53
+  }
 
+  await api.post('/api/blogs')
+  .send(newBlog)
+  .expect(201)
+  .expect('Content-Type', /application\/json/)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
