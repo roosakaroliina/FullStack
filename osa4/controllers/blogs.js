835b70eacb5beb48ffd05178b0b0b6de3dc9a1fa
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-08-12 12:00:57 +0300

    4.15 lähes valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -16,26 +18,40 @@ blogsRouter.get('/:id', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const body = new Blog(request.body)
-
-  if (!body.likes) {
-    body.likes = 0
-  }
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
+  const body = request.body
   try {
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  } catch (exception) {
-    next(exception)
+    const user = await User.findById(body.userId)
+
+    if (!user) {
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0
+
+      })
+      const savedBlog = await blog.save()
+      response.status(201).json(savedBlog)
+    } else {
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        user: user._id,
+      })
+
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.status(201).json(savedBlog)
+    }
+  } catch (error) {
+    next(error)
   }
 })
+
+
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
@@ -58,5 +74,6 @@ blogsRouter.put('/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
+
 
 module.exports = blogsRouter
