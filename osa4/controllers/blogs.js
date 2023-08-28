dc206266fd05f67089461e64deab34d91493e803
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-08-28 19:29:57 +0300

    tehtävä 4.19 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,6 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
@@ -19,9 +29,12 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   try {
-    const user = await User.findById(body.userId)
-
     if (!user) {
       const blog = new Blog({
         title: body.title,
