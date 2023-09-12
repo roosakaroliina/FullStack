5671dd369fadec51e965888d8e8b8b4183aeaeb1
Roosa Kuusivaara <roosa.kuusivaara@tuni.fi>
2023-09-12 15:07:23 +0300

    tehtävä 5.18 valmis

--- osa5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ osa5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -1,12 +1,35 @@
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     //cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:5173')
   })
 
-  it('Login form is shown', function() {
-    cy.get('input:first')
-    cy.get('input:last')
+
+  it('Login form is shown', function () {
+    cy.get('#username')
+    cy.get('#password')
     cy.contains('login')
   })
+  describe('Login', function () {
+    const user = {
+      username: 'root',
+      password: 'sekret'
+    }
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type(user.password)
+      cy.get('#login-button').click()
+
+      cy.contains('Pääkäyttäjä logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.contains('wrong username or password')
+    })
+  })
+
 })
